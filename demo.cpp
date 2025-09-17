1a5a9c70a5bae7f1eee8f51c0d5d0c991d0faeb0
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-17 18:23:02 +0530

    introduction to time and space complexity

--- demo.cpp
+++ demo.cpp
@@ -2,16 +2,10 @@
 
 using namespace std;
 
-int sum(int num1, int num2);
-
 int main()
 {
-    int num1, num2;
-    cin >> num1 >> num2;
-    cout << "The total = " << sum(num1, num2);
-}
-
-int sum(int num1, int num2)
-{
-    return num1 + num2;
+    int a, b;
+    cin >> a >> b;
+    int c = a + b;
+    cout << c;
 }
