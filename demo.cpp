fd343597e018e18b0c31f129ec618862017718d0
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-17 13:20:22 +0530

    introduction to arrays and strings

--- demo.cpp
+++ demo.cpp
@@ -4,37 +4,6 @@ using namespace std;
 
 int main()
 {
-    int day;
-    string dayName;
-    cin >> day;
-
-    switch (day)
-    {
-    case 1:
-        dayName = "Monday";
-        break;
-    case 2:
-        dayName = "Tuesday";
-        break;
-    case 3:
-        dayName = "Wednesday";
-        break;
-    case 4:
-        dayName = "Thursday";
-        break;
-    case 5:
-        dayName = "Friday";
-        break;
-    case 6:
-        dayName = "Saturday";
-        break;
-    case 7:
-        dayName = "Sunday";
-        break;
-    default:
-        dayName = "Invalid Day";
-        break;
-    }
-
-    cout << "Happy " << dayName;
+    string name = "abbys";
+    cout << name[0];
 }
