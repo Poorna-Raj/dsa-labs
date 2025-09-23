c361495822d3bfb0709d0cd241497bf9c98a8df6
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-23 13:41:08 +0530

    introduction to set in STL

--- demo.cpp
+++ demo.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <queue>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 int main()
 {
-    priority_queue<int, vector<int>, greater<int>> que;
-
-    que.push(4);
-    que.push(10);
-    que.push(6);
-    que.emplace(4);
-
-    cout << "Top is -> " << que.top() << endl;
+    unordered_set<int> set;
 }
