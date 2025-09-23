603e7a01d2ad8229c36c3fb7a496ac0565b50473
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-23 14:39:52 +0530

    introduction to maps in STL

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <unordered_set>
+#include <map>
 #include <vector>
 
 using namespace std;
 
 int main()
 {
-    unordered_set<int> set;
+    map<int, int> maps;
+
+    maps.emplace(2, 10);
+    maps.emplace(5, 50);
+    maps.insert({1, 6});
+
+    auto it = maps.find(5);
+    cout << it->second << endl;
 }
